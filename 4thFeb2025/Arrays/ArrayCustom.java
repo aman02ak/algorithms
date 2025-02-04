ef510e7293cf88e0eb98f0d7214508b1954cbcc5
Aman Kumar <amankr.02ak@gmail.com>
2025-02-04 23:29:07 +0530

    Update ArrayCustom.java - Added a new method to add new element at the beginning of the array

--- 4thFeb2025/Arrays/ArrayCustom.java
+++ 4thFeb2025/Arrays/ArrayCustom.java
@@ -1,5 +1,15 @@
 public class ArrayCustom {
     private ArrayCustom () {}
 
-    
+    // adding new item to beginning of a array of int type
+    // Input: an array, new element
+    // Output: returns new modified array, doesn't modify original array
+    public static int[] addItemToBeginning (int[] arr, int element) {
+        int[] tempArr = Arrays.copyOf (arr, arr.length + 1);
+        
+        tempArr[0] = element;
+        System.arraycopy (arr, 0, tempArr, 1, arr.length);
+
+        return tempArr;
+    }    
 }
